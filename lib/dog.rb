c19862f3a2405f179c60b67bec9facfe76de3479
Paul <paul.ngugi@student.moringaschool.com>
2023-02-08 19:36:40 +0300

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
-
+    # setter method
+    def name=(dog_name)
+        # assign the @name instance variable a value of whatever is passed as an argument
+        @this_dogs_name = dog_name
+    end
+    # getter method
+    def name
+        # access the data from the @this_dogs_name instance variable and return it
+        @this_dogs_name
+    end
 end
+
+lassie = Dog.new
+
+# call the Dog#name= method with a value of "Lassie" = lassie.name=("Lassie")
+lassie.name = "Lassie"
+
+# call the Dog#name method = "Lassie"
+puts lassie.name
